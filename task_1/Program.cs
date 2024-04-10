260a58577c7faac357a36dda90521a20268a5581
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-10 20:30:08 +0300

    modified test

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 Console.WriteLine("  HELLO WORLD");
 Console.WriteLine("  TEST  TEST TEST TEST");
+Console.WriteLine("  TEST  TEST TEST TEST");
 Console.WriteLine("  HELLO WORLD");
 Console.WriteLine("  HELLO WORLD");
--- task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
+++ task_1/obj/Debug/net8.0/task_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21586e7a4faa782ab945ee27279772aaedf8083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3afdb80a29406b093887fe1fc4c19286ce0d14a")]
 [assembly: System.Reflection.AssemblyProductAttribute("task_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
