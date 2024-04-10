339cca9e5eca8a68adb9c3605e17346d78330e4c
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-10 20:27:08 +0300

    added text

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,4 +1,6 @@
 ﻿
 Console.WriteLine("  HELLO WORLD");
 Console.WriteLine("  HELLO WORLD");
+Console.WriteLine("Я могу помочь вам открыть для себя новые места,"+ 
+ "создать маршруты и помочь забронировать путешествия");
 Console.WriteLine("  HELLO WORLD");
