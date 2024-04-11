10df8ce12b51fe19725a2bbce9b862c95434e81d
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-11 14:24:29 +0300

    added some variable: int, double, char and boolean

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,3 +1,10 @@
 ﻿
 int value = 100;
 Console.WriteLine($"  Value == {value}");
+double d = 5.55;
+char c ='!';
+bool b = true;
+
+Console.WriteLine($"  Double == {d}");
+Console.WriteLine($"  Char == {c}");
+Console.WriteLine($"  Boolean == {b}");
