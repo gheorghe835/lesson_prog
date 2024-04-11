117c81191ab7f148723d48f9597a6fb7117d1209
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-11 19:48:49 +0300

    added task_3

---
+++ task_3/Program.cs
@@ -0,0 +1,9 @@
+﻿
+int val1 = 5;
+int val2 = 6;
+int summ = val1 + val2;
+
+Console.WriteLine($"  Summa == {val1} + {val2} == {summ}");
+
+var val3 = "string";
+Console.WriteLine($"  Value3 == {val3} + {val3} == {val3}");
