30cb36f01dbe89203171ab6efdbc2cf199b4f014
GeorgeBatiri <gheorghe835@gmail.com>
2024-04-12 19:13:19 +0300

    added array

---
+++ task_6/Program.cs
@@ -0,0 +1,32 @@
+﻿
+Console.Write($" Enter the number N :: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($" \n  The number N :: {N}");
+int[] array1 = new int[N];
+int i = 0;
+int max = array1[0];
+while( i < N)
+{
+    array1[i] = i ;
+    Console.WriteLine($"  The number i :: {array1[i]}");
+    if(array1[i] > max)
+    {
+        max = array1[i];
+    }
+    i ++;
+}
+Console.WriteLine($"  The number maximum :: {max}");
+Console.WriteLine($"-----------------------------");
+for ( i = 0; i < N; i++)
+{
+    array1[i] = i ;
+}
+for ( i = 0; i < N; i++)
+{
+    Console.WriteLine($"  The number i :: {array1[i]}");
+}
+Console.WriteLine($"-----------------------------");
+foreach (var item in array1)
+{
+    Console.WriteLine($"   {item} ");
+}
